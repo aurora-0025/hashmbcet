37f7dee78e06802d6dc60f18784dd434dc2f432e
Ritin George <ritingeorge.d@gmail.com>
2022-10-24 22:39:10 +0530

    Events Page Setup

--- src/hash/events/components/eventcard.jsx
+++ src/hash/events/components/eventcard.jsx
@@ -1,14 +1,32 @@
+import HUIButton from "../../../components/button/button";
+
 function EventCard({ event }) {
     return (
+
         <div className="eventCard">
-            {event.name}
-            <div className="datecontainer">
-                {event.date.month}
-                <div className="datenumbers">
-                    <div className="date">{event.date.day}</div>
+            <div className="dateTimeContainer">
+                <div className="datecontainer">
+                    {event.date.month}
+                    <div className="datenumbers">
+                        <div className="date">{event.date.day}</div>
+                    </div>
                 </div>
+                <div className="timecontainer">{event.time}</div>
             </div>
-            <div className="timecontainer">{event.time}</div>
+            <div className="eventdata">
+                <h3 className="eventname">{event.name}</h3>
+                <p className="eventevent"> {event.event}</p>
+                <p className="eventdescription">{event.description}</p>
+                <HUIButton
+                    text="Register Now"
+                    icon="go"
+                    event="hash"
+                    type="primary"
+                    variant="2"
+                    color="var(--beurocratic-blue)"
+                    // link="https://www.rebrand.ly/define_sponsorguide"
+                ></HUIButton>
+            </div>
         </div>
     );
 }
--- src/hash/events/events.jsx
+++ src/hash/events/events.jsx
@@ -1,31 +1,75 @@
+import Carousel from '../../components/carousel/carousel';
 import EventCard from './components/eventcard';
 import './events.css';
 
-const events = [
-    {
-        name: 'Block Chain Workshop',
-        date: {
-            day: 5,
-            month: 'nov',
-        },
-        time: '6PM',
-    },
-    {
-        name: 'Valorant',
-        date: {
-            day: 6,
-            month: 'nov',
-        },
-        time: '2:50PM',
-    },
-];
 
 function Events() {
+    const events = [
+        {
+            name: 'Define.',
+            event: 'Hackathon',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias atque eligendi' ,
+            date: {
+                day: 5,
+                month: 'nov',
+            },
+            time: '6:00PM',
+        },
+        {
+            name: 'Rising',
+            event: 'Valorant Tournament',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias atque eligendi' ,
+            date: {
+                day: 6,
+                month: 'nov',
+            },
+            time: '2:50PM',
+        },
+        {
+            name: 'Battle of Codes',
+            event: 'Competitive Coding',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias atque eligendi' ,
+            date: {
+                day: 6,
+                month: 'nov',
+            },
+            time: '2:50PM',
+        },
+        {
+            name: 'Experia',
+            event: 'Project Expo',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias atque eligendi' ,
+            date: {
+                day: 6,
+                month: 'nov',
+            },
+            time: '2:50PM',
+        },
+        {
+            name: 'Spark',
+            event: 'Idea Presentation',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias atque eligendi' ,
+            date: {
+                day: 6,
+                month: 'nov',
+            },
+            time: '2:50PM',
+        },
+    ];
+
+
     return (
         <div className="events">
-            {events.map((e) => (
-                <EventCard event={e} />
-            ))}
+            <h1>Here's How.</h1>
+            <div>
+                <Carousel>
+                    {events.map((e) => (
+                        <div>
+                            <EventCard event={e} />
+                        </div>
+                    ))}
+                </Carousel>
+            </div>
         </div>
     );
 }
