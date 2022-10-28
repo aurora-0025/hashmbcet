c25180db8f7dfc5429548eb9b6e4f27486616d32
Ritin George <78461388+aurora-0025@users.noreply.github.com>
2022-10-29 02:41:39 +0530

    responsiveness fix 2

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -16,14 +16,22 @@ function Carousel({children}) {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
-    centerPadding: '0%',
+    centerPadding: '10%',
     responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerPadding: '10%',
+        }
+      },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-          centerPadding: '0%',
+          centerPadding: '7%',
         }
       },
       {
@@ -68,7 +76,7 @@ function Carousel({children}) {
           slidesToShow: 1,
           slidesToScroll: 1,
           centerMode: true,
-          centerPadding: '12%',
+          centerPadding: '10%',
         }
       },
       {
